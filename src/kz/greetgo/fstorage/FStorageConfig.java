ef64b13200d9430296ecf4f230aaa2f1ed047db3
ekolpakov <ekolpakov@greet-go.com>
2018-02-22 10:02:58 +0600

    Подготовлена версия 1.0.0

--- src/kz/greetgo/fstorage/FStorageConfig.java
+++ src/kz/greetgo/fstorage/FStorageConfig.java
@@ -3,11 +3,12 @@ package kz.greetgo.fstorage;
 public class FStorageConfig {
   public final String tableName;
   public final int tableCount;
-  
-  public boolean hasCreatedAt = false;
-  
-  public FStorageConfig(String tableName, int tableCount) {
+
+  public final boolean hasCreatedAt;
+
+  public FStorageConfig(String tableName, int tableCount, boolean hasCreatedAt) {
     this.tableName = tableName;
     this.tableCount = tableCount;
+    this.hasCreatedAt = hasCreatedAt;
   }
 }
--- test_src/kz/greetgo/fstorage/FStorageTest.java
+++ test_src/kz/greetgo/fstorage/FStorageTest.java
@@ -1,65 +1,65 @@
 package kz.greetgo.fstorage;
 
-import static org.fest.assertions.api.Assertions.assertThat;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import javax.sql.DataSource;
-
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import static org.fest.assertions.api.Assertions.assertThat;
 
 public class FStorageTest extends MyTestBase {
-  
+
   private static class LocalFileDot {
     long id;
     String filename;
     byte[] data;
   }
-  
+
   private static final String ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   private static final String abc = "abcdefghijklmnopqrstuvwxyz";
   private static final String DEG = "0123456789";
   private static final String ALL_ENG = ABC + abc + DEG;
-  
-  private static final String rndStr(int len, Random rnd) {
+
+  @SuppressWarnings("SameParameterValue")
+  private static String rndStr(int len, Random rnd) {
     StringBuilder sb = new StringBuilder();
     for (int i = 0; i < len; i++) {
       sb.append(ALL_ENG.charAt(rnd.nextInt(ALL_ENG.length())));
     }
     return sb.toString();
   }
-  
-  private static final byte rndByte(Random rnd) {
-    return (byte)rnd.nextInt(256);
+
+  private static byte rndByte(Random rnd) {
+    return (byte) rnd.nextInt(256);
   }
-  
+
   @DataProvider
   public Object[][] addNewFile_getFile_DP() throws Exception {
-    
+
     List<Object[]> ret = new ArrayList<>();
-    
+
     for (Object[] mas : dataProvider()) {
-      Object dataSorce = mas[0];
-      
-      ret.add(new Object[] { dataSorce, false });
-      ret.add(new Object[] { dataSorce, true });
-      
+      Object dataSource = mas[0];
+
+      ret.add(new Object[]{dataSource, false});
+      ret.add(new Object[]{dataSource, true});
+
     }
-    
+
     return ret.toArray(new Object[ret.size()][]);
   }
-  
+
   @Test(dataProvider = "addNewFile_getFile_DP")
   public void addNewFile_getFile(DataSource dataSource, boolean hasCreatedAt) throws Exception {
     Random rnd = new Random();
-    
+
     final List<LocalFileDot> lfdList = new ArrayList<>();
-    
+
     for (int i = 0; i < 110; i++) {
       LocalFileDot x = new LocalFileDot();
       x.filename = rndStr(10, rnd);
@@ -69,21 +69,20 @@ public class FStorageTest extends MyTestBase {
       }
       lfdList.add(x);
     }
-    
+
     FStorageFactory f = new FStorageFactory();
     f.setDataSource(dataSource);
-    FStorageConfig config = new FStorageConfig("test_file_table", 10);
-    config.hasCreatedAt = hasCreatedAt;
+    FStorageConfig config = new FStorageConfig("test_file_table", 10, hasCreatedAt);
     f.setConfig(config);
-    
+
     dropAllTables(dataSource.getConnection(), "test_file_table", 10);
-    
+
     FStorage fs = f.create();
-    
+
     for (LocalFileDot lfd : lfdList) {
       lfd.id = fs.addNewFile(new FileDot(lfd.filename, lfd.data));
     }
-    
+
     for (LocalFileDot lfd : lfdList) {
       FileDot fd = fs.getFile(lfd.id);
       assertThat(fd).isNotNull();
@@ -94,10 +93,10 @@ public class FStorageTest extends MyTestBase {
       }
     }
   }
-  
+
   private void dropAllTables(Connection con, String tableSuffix, int tableCount)
-      throws SQLException {
-    
+    throws SQLException {
+
     int size = 0;
     {
       int a = tableCount;
@@ -106,21 +105,21 @@ public class FStorageTest extends MyTestBase {
         a = a / 10;
       }
     }
-    
+
     for (int i = 0; i < tableCount; i++) {
-      
+
       String nom = "" + i;
       while (nom.length() < size) {
         nom = "0" + nom;
       }
-      
+
       String tn = tableSuffix + tableCount + '_' + nom;
-      
+
       queryForce(con, "drop table " + tn);
     }
-    
+
     queryForce(con, "drop sequence " + tableSuffix + "_seq");
-    
+
     con.close();
   }
 }
